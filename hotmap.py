23742d41bb2d75a13f40b9ad51c879902b2a0873
hik <105617575+peixyhe@users.noreply.github.com>
2024-03-04 16:21:36 +0800

    Update hotmap.py

--- hotmap.py
+++ hotmap.py
@@ -8,8 +8,9 @@ down = 21.8    #                           up(-90.0, 90.0)
 left = 97.8    #     left(-180.0, 180.0)                     right(-180.0, 180.0)
 right = 107    #                          down(-90.0, 90.0)
 
-step = 0.05
+step = 0.01
 scale = 10
+window_Width = 5
 
 
 
@@ -22,6 +23,8 @@ from scipy.spatial import cKDTree
 half_setp = round(  step * 0.5, 5  )
 ANGLE2METERS = 2 * math.pi * 6371393.0 / 360.0    # EARTH_RADIUS = 6371393.0
 z0 = scale * 6000 / ANGLE2METERS
+search_radius = window_Width * step / 2.0
+core_radius = search_radius * 1.5
 
 colors = vtk.vtkNamedColors()
 points = vtk.vtkPoints()
@@ -44,31 +47,26 @@ x = [  round(x0, 5) for x0 in np.arange(left, right + half_setp, step)  ]
 y = [  round(y0, 5) for y0 in np.arange(down, up + half_setp, step)  ]
 
 for yj in y:
+    print(yj)
     for xi in x:
         points.InsertNextPoint(xi, yj, z0)
+        freq0 = 0
+        mag0 = 0.0
         
-        indices = data_kdTree.query_ball_point(  [xi, yj], r = step  )
+        indices = data_kdTree.query_ball_point(  [xi, yj], r = core_radius  )
         if indices is not None:
             hotmap_list = []
-            mag0 = 0.0
             
             for index in indices:
                 p = data[index]
-                if p[0] >= xi - half_setp and p[0] < xi + half_setp and p[1] >= yj - half_setp and p[1] < yj + half_setp:
+                if (p[0] >= xi - search_radius) and (p[0] < xi + search_radius) and (p[1] >= yj - search_radius) and (p[1] < yj + search_radius):
                     hotmap_list.append(index)
                     if p[2] > mag0:
                         mag0 = p[2]
+            freq0 = len(hotmap_list)
             
-            if hotmap_list != []:
-                freq.InsertNextValue(  len(hotmap_list)  )
-                max_mag.InsertNextValue(  mag0  )
-            else:
-                freq.InsertNextValue(  -1  )
-                max_mag.InsertNextValue(  -1  )
-
-        else:
-            freq.InsertNextValue(  -1  )
-            max_mag.InsertNextValue(  -1  )
+        freq.InsertNextValue(freq0)
+        max_mag.InsertNextValue(mag0)
 
 x_num = len(x)
 y_num = len(y)
