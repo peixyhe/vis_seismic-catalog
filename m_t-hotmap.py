97759cd92f85edc803aa7799e139118c2c628546
hik <105617575+peixyhe@users.noreply.github.com>
2024-03-08 12:36:06 +0800

    Update and rename m_t-f.py to m_t-hotmap.py

--- m_t-hotmap.py
+++ m_t-hotmap.py
@@ -1,33 +1,19 @@
-###############################################################################
-####  The program is used to polt M-T-F diagrams of seismic catalog data.  ####
-####  Author: He Pei; 2024.02.25                                           ####
-###############################################################################
+######################################################################################
+####  The program is used to polt M-T-F_hotmap diagrams of seismic catalog data.  ####
+####  Author: He Pei; 2024.02.25                                                  ####
+######################################################################################
 
 
 
-num_mouths = 3
-
-year0 = 2009
-year1 = 2022
-mag_step = 0.05
-t_step = 0.05
-
-if num_mouths == 1:
-    minification = 20.0
-    # t_step = 0.05 * 6.0
-elif num_mouths == 3:
-    minification = 30.0
-    # t_step = 0.05 * 2.0
-elif num_mouths == 6:
-    minification = 50.0
-    # t_step = 0.05
-else:
-    minification = 10.0
-    # t_step = 0.05
+step_mouths = 5
+min_mag = 1.0
 
 
+
+step_mag = 0.1
 
 import vtk
+# import math
 import pandas as pd
 import numpy as np
 from scipy.spatial import cKDTree
@@ -37,68 +23,68 @@ def seconds_between_times(time1, time2):
     time1_dt = datetime.strptime(time1, '%Y.%m.%d %H:%M')    # time1_dt = datetime.strptime(time1, '%Y-%m-%d %H:%M:%S')
     time2_dt = datetime.strptime(time2, '%Y.%m.%d %H:%M')
     
-    seconds_diff = (time2_dt - time1_dt).total_seconds()
-    
-    return abs(seconds_diff) / (3600.0 * 24 * 30 * num_mouths)    # 3600.0 * 24 * 30 * 6 = seconds for 6 mouths
+    return abs(  (time2_dt - time1_dt).total_seconds()  ) / (3600.0 * 24 * 30 * step_mouths)
 
 colors = vtk.vtkNamedColors()
 ugrid = vtk.vtkUnstructuredGrid()
-
 points = vtk.vtkPoints()
-
-freq = vtk.vtkFloatArray()
-freq.SetNumberOfComponents(1)
-freq.SetName('frequency')
 
 max_mag = vtk.vtkFloatArray()
 max_mag.SetNumberOfComponents(1)
 max_mag.SetName('MAX magnitude')
 
+freq = vtk.vtkFloatArray()
+freq.SetNumberOfComponents(1)
+freq.SetName('frequency')
+
 df = pd.read_csv("data\\mag(1_7.2)-2009_2021.csv")
 time = df['time'].to_list()
 mag = df['mag']
 
-t_min = time[-1]
-t_max = time[0]
-
 x0 = 0.0
-x1 = seconds_between_times(t_min, t_max)
-y0 = min(mag)
+x1 = seconds_between_times("2009.1.1 0:0", "2021.12.31 23:59")
+# y0 = min(mag)
+y0 = min_mag
 y1 = max(mag)
 
+year0 = 2009
+year1 = 2022
 k = (year1 - year0) / (x1 - x0)
-b = year0 - k * x0
-
-half_mag_setp = round(  mag_step * 0.5, 5  )
-half_t_step = round(    t_step * 0.5, 5    )
-r_step = half_mag_setp + half_t_step
+print(1 / k, -1.0 / k * 2009)
 
 xy = []
-for i in range(len(mag)):
-    xi = seconds_between_times(  t_min, time[i]  )
-    xy.append(  [xi, mag[i] ]  )
-
+for i in range(  len(mag)  ):
+    if mag[i] >= min_mag:
+        xi = seconds_between_times(  "2009.1.1 0:0", time[i]  )
+        xy.append(    [  xi, mag[i] ]    )
 xy_kdTree = cKDTree(xy)
 
-x = [    round(xx, 5) for xx in np.arange(x0, x1 + half_t_step,   t_step)    ]
-y = [    round(yy, 5) for yy in np.arange(y0, y1 + half_mag_setp, mag_step)    ]
+half_mag_step = step_mag * 0.5
+x_step = step_mag
+half_x_step = x_step * 0.5
+kdTree_radius = half_x_step + half_mag_step
+
+x = [    round(xx, 5) for xx in np.arange(x0, x1 + half_x_step, x_step)    ]
+y = [    round(yy, 5) for yy in np.arange(y0, y1 + half_mag_step, step_mag)    ]
 
 for yj in y:
     for xi in x:
         p_list = []
         mag0 = 0.0
-        indices = xy_kdTree.query_ball_point(  [xi, yj], r = r_step  )
+        indices = xy_kdTree.query_ball_point(  [xi, yj], r = kdTree_radius  )
         
         for index in indices:
             p = xy[index]
-            if (p[0] >= xi - half_t_step) and (p[0] < xi + half_t_step) and (p[1] >= yj - half_mag_setp) and (p[1] < yj + half_mag_setp):
+            if (p[0] >= xi - half_x_step) and (p[0] < xi + half_x_step) and (p[1] >= yj - half_mag_step) and (p[1] < yj + half_mag_step):
                 p_list.append(index)
                 if p[1] > mag0:
                     mag0 = p[1]
-                    
-        points.InsertNextPoint(    k * xi + b, yj, len(p_list) / minification    )
-        freq.InsertNextValue(  len(p_list)  )
+
+        points.InsertNextPoint(    xi, yj, 0.0    )
+        freq.InsertNextValue(    len(p_list) + 1    )
         max_mag.InsertNextValue(mag0)
+
+
 
 x_num = len(x)
 y_num = len(y)
@@ -126,7 +112,7 @@ ugrid.GetPointData().AddArray(max_mag)
 
 writer = vtk.vtkUnstructuredGridWriter()
 writer.SetInputData(ugrid)
-writer.SetFileName(  "m_t_f-" + str(num_mouths) + "_mouths.vtu"  )
+writer.SetFileName(  "hotmap_M-T_minMAG" + str(min_mag) + "_" + str(step_mouths) + "mouths.vtk"  )
 # writer.SetDataModeToAscii()
 writer.Update()
 
